e08c8e11b28c9d31d61a25484995aef56ef40114
Gerard Montane <gmontanee@gmail.com>
2019-07-31 12:15:17 +0200

    Can answer questions

--- routes/home.js
+++ routes/home.js
@@ -9,58 +9,46 @@ const Question = require('../models/Questions');
 
 /* GET users listing. */
 router.get('/', isNotLoggedIn, (req, res, next) => {
-  const user = req.session.currentUser;
-  User.findById(user._id).populate('questionsMade')
-    .then((userData) => {
-      req.session.currentUser = userData;
-    }).catch(err => {
-      console.log(err);
-    });
-  Question.find()
-    .then((questions) => {
-      const arrOfQuestions = [];
-      for (let i = questions.length - 1; i > 0 && i >= questions.length - 20; i--) {
-        const question = arrayOfAnswers(questions[i]);
-        console.log(question);
-        arrOfQuestions.push(question);
-      }
-      // const { id } = req.params;
-      // const newQuestion = await Question.findById(id);
-      // const questionObject = arrayOfAnswers(newQuestion, id);
-      // res.render('home', questionObject);
-      // console.log(questions);
-      res.render('home', arrOfQuestions);
-    }).catch(err => {
-      console.log(err);
-    });
+  // const user = req.session.currentUser;
+  // User.findById(user._id).populate('questionsMade')
+  //   .then((userData) => {
+  //     req.session.currentUser = userData;
+  //   }).catch(err => {
+  //     console.log(err);
+  //   });
+  if (req.session.currentUser.generatedQuestions.length === 0) {
+    console.log(req.session.currentUser.generatedQuestions);
+    Question.find()
+      .then((questions) => {
+        const arrOfQuestions = [];
+        for (let i = questions.length - 1; i > 0 && i >= questions.length - 20; i--) {
+          const question = arrayOfAnswers(questions[i]);
+          arrOfQuestions.push(question);
+        }
+        req.session.currentUser.generatedQuestions = arrOfQuestions;
+        res.render('home', arrOfQuestions);
+      }).catch(err => {
+        console.log(err);
+      });
+  } else {
+    res.render('home', req.session.currentUser.generatedQuestions);
+  }
+});
+
+router.post('/:id', isNotLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  const question = req.session.currentUser.generatedQuestions.find(elem => elem._id == id);
+  // console.log(question);
+  // console.log(question.answers.find(elem => elem.correct == true).solution);
+  if (question.answers.find(elem => elem.correct == true).solution == req.body.solution) {
+    console.log('correct');
+  } else {
+    console.log('wrong');
+  }
+  // const questionsArray = req.session.currentUser.generatedQuestions;
+  // console.log(req.body.solution);
+
+  res.redirect('/home');
 });
 
 module.exports = router;
-
-// const arrayOfPositions = [];
-// const arrayOfValues = [];
-// for (let i = 0; i < newQuestion.parameters.length; i++) {
-//   arrayOfPositions.push(newQuestion.parameters[i].position);
-//   arrayOfValues.push(randomNumber(newQuestion.parameters[i].max, newQuestion.parameters[i].min, newQuestion.parameters[i].precission));
-// }
-// question = replaceVar(question, arrayOfPositions, arrayOfValues);
-
-// const parser = new Parser();
-// const expr = parser.parse(`${newQuestion.function}`);
-// const solution = expr.evaluate({
-//   a: arrayOfValues[0],
-//   b: arrayOfValues[1],
-//   c: arrayOfValues[2],
-//   d: arrayOfValues[3],
-//   e: arrayOfValues[4],
-//   f: arrayOfValues[5],
-//   g: arrayOfValues[6],
-//   i: arrayOfValues[7]
-// });
-
-// const questionObject = {
-//   question,
-//   variables: arrayOfValues,
-//   functionAnswer,
-//   solution
-// };
